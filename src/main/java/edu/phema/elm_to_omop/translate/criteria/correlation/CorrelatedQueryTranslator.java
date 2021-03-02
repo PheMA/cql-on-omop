560cee3bb4acc6525176ba2f8d57226cae030e8e
Pascal Brandt <psbrandt@gmail.com>
2021-03-02 14:20:38 -0800

    🤠 Add support for more types of correlated queries (#50)
    * 🤠 Add support for more types of correlated queries.
    
    * Fix for failing legacy tests
    
    Co-authored-by: Luke Rasmussen <luke.rasmussen@gmail.com>

--- src/main/java/edu/phema/elm_to_omop/translate/criteria/correlation/CorrelatedQueryTranslator.java
+++ src/main/java/edu/phema/elm_to_omop/translate/criteria/correlation/CorrelatedQueryTranslator.java
@@ -1,10 +1,15 @@
 package edu.phema.elm_to_omop.translate.criteria.correlation;
 
+import edu.phema.elm_to_omop.helper.CirceUtil;
+import edu.phema.elm_to_omop.translate.CorelatedCriteriaTranslator;
+import edu.phema.elm_to_omop.translate.CriteriaGroupTranslator;
+import edu.phema.elm_to_omop.translate.PhemaElmToOmopTranslator;
 import edu.phema.elm_to_omop.translate.PhemaElmToOmopTranslatorContext;
 import edu.phema.elm_to_omop.translate.exception.PhemaNotImplementedException;
 import edu.phema.elm_to_omop.translate.exception.PhemaTranslationException;
 import org.hl7.elm.r1.*;
 import org.ohdsi.circe.cohortdefinition.CorelatedCriteria;
+import org.ohdsi.circe.cohortdefinition.CriteriaGroup;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -40,8 +45,8 @@ public class CorrelatedQueryTranslator {
    * @return A map from aliases to QuickResource instances
    */
   private static Map<String, QuickResource> setupAliases(Query query,
-      PhemaElmToOmopTranslatorContext context)
-      throws CorrelationException, PhemaTranslationException {
+                                                         PhemaElmToOmopTranslatorContext context)
+    throws CorrelationException, PhemaTranslationException {
     Map<String, QuickResource> aliases = new HashMap<>();
 
     AliasedQuerySource outerAliasedExpression = query.getSource().get(0);
@@ -55,9 +60,9 @@ public class CorrelatedQueryTranslator {
     String innerValuesetFilter = context.getVocabularyReferenceForRetrieve(innerRetrieve);
 
     aliases.put(outerAliasedExpression.getAlias(),
-        QuickResource.from(outerResourceType, outerValuesetFilter));
+      QuickResource.from(outerResourceType, outerValuesetFilter));
     aliases.put(innerAliasedExpression.getAlias(),
-        QuickResource.from(innerResourceType, innerValuesetFilter));
+      QuickResource.from(innerResourceType, innerValuesetFilter));
 
     return aliases;
   }
@@ -72,15 +77,15 @@ public class CorrelatedQueryTranslator {
    * correlation
    */
   private static QuickResourceAttributePair getResourceAttributePairFromOperand(Expression operand,
-      Map<String, QuickResource> aliases) throws CorrelationException {
+                                                                                Map<String, QuickResource> aliases) throws CorrelationException {
     if (operand instanceof Property) {
       return new QuickResourceAttributePair(aliases.get(((Property) operand).getScope()),
-          CorrelationConstants.QuickResourceAttribute.create(((Property) operand).getPath()));
+        CorrelationConstants.QuickResourceAttribute.create(((Property) operand).getPath()));
     } else if (operand instanceof As) {
       return getResourceAttributePairFromOperand(((As) operand).getOperand(), aliases);
     } else {
       throw new CorrelationException(
-          "Correlation not supported for operand type: " + operand.getClass().getSimpleName());
+        "Correlation not supported for operand type: " + operand.getClass().getSimpleName());
     }
   }
 
@@ -92,52 +97,96 @@ public class CorrelatedQueryTranslator {
    * @return The created Correlation object
    */
   private static Correlation getCorrelation(Query query, PhemaElmToOmopTranslatorContext context)
-      throws PhemaNotImplementedException, CorrelationException, PhemaTranslationException {
+    throws PhemaNotImplementedException, CorrelationException, PhemaTranslationException {
     Expression correlationExpression = query.getRelationship().get(0).getSuchThat();
 
     if (!correlationExpressionSupported(correlationExpression)) {
       throw new PhemaNotImplementedException(String
-          .format("Correlation not support for %s expression",
-              correlationExpression.getClass().getSimpleName()));
+        .format("Correlation not support for %s expression",
+          correlationExpression.getClass().getSimpleName()));
     }
 
     Map<String, QuickResource> aliases = setupAliases(query, context);
 
     if (correlationExpression instanceof Equal) {
       QuickResourceAttributePair lhs = getResourceAttributePairFromOperand(
-          ((Equal) correlationExpression).getOperand().get(0), aliases);
+        ((Equal) correlationExpression).getOperand().get(0), aliases);
       QuickResourceAttributePair rhs = getResourceAttributePairFromOperand(
-          ((Equal) correlationExpression).getOperand().get(1), aliases);
+        ((Equal) correlationExpression).getOperand().get(1), aliases);
 
       return new Correlation(lhs, rhs, correlationExpression);
     } else {
       throw new PhemaNotImplementedException(String
-          .format("Correlation not support for %s expression",
-              correlationExpression.getClass().getSimpleName()));
+        .format("Correlation not support for %s expression",
+          correlationExpression.getClass().getSimpleName()));
     }
   }
 
   /**
    * Creates a Circe CorelationCriteria from an ELM correlated Query expression
    *
-   * @param query The correlated Query expression
+   * @param query   The correlated Query expression
    * @param context The translation context
    * @return The created CorelationCriteria
    */
   public static CorelatedCriteria generateCorelatedCriteriaForCorrelatedQuery(Query query,
-      PhemaElmToOmopTranslatorContext context) throws
-      PhemaNotImplementedException, CorrelationException, PhemaTranslationException {
+                                                                              PhemaElmToOmopTranslatorContext context) throws
+    PhemaNotImplementedException, CorrelationException, PhemaTranslationException {
 
     Correlation correlation = getCorrelation(query, context);
 
     CorrelatedQueryCorelatedCriteriaGeneratorFunction<Correlation, PhemaElmToOmopTranslatorContext, CorelatedCriteria> generator = CorrelationConstants.generators
-        .get(correlation);
+      .get(correlation);
 
     if (generator == null) {
       throw new CorrelationException(
-          String.format("Unsupported correlation: %s", correlation.toString()));
+        String.format("Unsupported correlation: %s", correlation.toString()));
     } else {
       return generator.apply(correlation, context);
     }
+  }
+
+  public static boolean correlationWhereExpressionSupported(Expression expression) {
+    return expression instanceof Exists || PhemaElmToOmopTranslator.isBooleanExpression(expression) || expression instanceof In;
+  }
+
+  public static CorelatedCriteria generateCorelatedCriteriaForCorrelatedQueryWithWhere(Query query, PhemaElmToOmopTranslatorContext context) throws
+    Exception {
+    Expression whereExpression = query.getWhere();
+
+    if (!correlationWhereExpressionSupported(whereExpression)) {
+      throw new CorrelationException(String.format("Unsupported correlation: %s", whereExpression.getClass().getSimpleName()));
+    }
+
+    // Push the alias to the context so we can access it in the nested expression
+    String alias = context.addAlias(query);
+
+    CorelatedCriteria outerCorrelateCriteria = CirceUtil.defaultCorelatedCriteria();
+
+    if (whereExpression instanceof Exists || PhemaElmToOmopTranslator.isBooleanExpression(whereExpression)) {
+      // descend into the nested expression
+      CriteriaGroup criteriaGroup = CriteriaGroupTranslator.generateCriteriaGroupForExpression(whereExpression, context);
+
+      outerCorrelateCriteria = CorelatedCriteriaTranslator.generateCorelatedCriteriaForExpression((Retrieve) query.getSource().get(0).getExpression(), context);
+
+      outerCorrelateCriteria.criteria.CorrelatedCriteria = criteriaGroup;
+
+    } else if (whereExpression instanceof In) {
+      // we are in the leaf expression (right now we only support "in Interval")
+
+      // generate the criteria as if it was a retrieve
+      outerCorrelateCriteria = CorelatedCriteriaTranslator.generateCorelatedCriteriaForExpression((Retrieve) query.getSource().get(0).getExpression(), context);
+
+      // then set up the occurrence dates based on the interval
+      // TODO: Actually set up startWindow based on Interval - https://github.com/PheMA/cql-on-omop/issues/47
+      //       We can probably make a lot of simplifying assumptions here, for example that all dates/durations are relative
+      //       to the index date.
+    }
+
+
+    // Pop the alias to invalidate it
+    context.removeAlias(alias);
+
+    return outerCorrelateCriteria;
   }
 }
